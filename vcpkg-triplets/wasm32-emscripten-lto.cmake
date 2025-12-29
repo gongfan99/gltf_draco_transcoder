384c444668929812273f5271fe719d245dc46962
Fan Gong <gongfan99@hotmail.com>
2025-12-29 16:36:25 -0500

    modified:   .github/workflows/build_wasm.yml 	modified:   CMakeLists.txt 	modified:   vcpkg-triplets/wasm32-emscripten-lto.cmake

--- vcpkg-triplets/wasm32-emscripten-lto.cmake
+++ vcpkg-triplets/wasm32-emscripten-lto.cmake
@@ -2,7 +2,9 @@ set(VCPKG_TARGET_ARCHITECTURE wasm32)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
-# Add LTO flags to all builds
+
+# Use a standard CMake variable that you will pass from the command line
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${EMSDK_PATH}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+
 set(VCPKG_CXX_FLAGS "-flto")
 set(VCPKG_C_FLAGS "-flto")
