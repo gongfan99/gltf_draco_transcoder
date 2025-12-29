09da4a2d786e3ce1e8cbb8c676547bb9a317e32f
Fan Gong <gongfan99@hotmail.com>
2025-12-29 16:44:19 -0500

    modified:   .github/workflows/build_wasm.yml 	modified:   vcpkg-overlays/draco/portfile.cmake 	modified:   vcpkg-triplets/wasm32-emscripten-lto.cmake

--- vcpkg-triplets/wasm32-emscripten-lto.cmake
+++ vcpkg-triplets/wasm32-emscripten-lto.cmake
@@ -3,8 +3,11 @@ set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
 
-# Use a standard CMake variable that you will pass from the command line
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${EMSDK_PATH}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+# 1. Tell vcpkg to "let through" the EMSDK variable from the shell
+list(APPEND VCPKG_ENV_PASSTHROUGH "EMSDK")
+
+# 2. Now $ENV{EMSDK} will actually have a value inside this script
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
 
 set(VCPKG_CXX_FLAGS "-flto")
 set(VCPKG_C_FLAGS "-flto")
