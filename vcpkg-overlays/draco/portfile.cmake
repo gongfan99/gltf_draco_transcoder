3074abf06587caa748c58d64ea0bfb86b1a95247
Fan Gong <gongfan99@hotmail.com>
2025-12-30 15:59:28 -0500

    modified:   .github/workflows/build_wasm.yml 	modified:   .github/workflows/build_wheels.yml 	deleted:    vcpkg-overlays/draco/enable-dedup.patch 	deleted:    vcpkg-overlays/draco/fix-invalid-casting.patch 	new file:   vcpkg-overlays/draco/fix-various-things.patch 	modified:   vcpkg-overlays/draco/portfile.cmake

--- vcpkg-overlays/draco/portfile.cmake
+++ vcpkg-overlays/draco/portfile.cmake
@@ -3,29 +3,12 @@ vcpkg_from_git(
     URL https://github.com/google/draco.git
     REF 3abbc66fdf5597b1560c44ce7840aac76900b3f7
     PATCHES
-        enable-dedup.patch
-        fix-invalid-casting.patch
+        fix-various-things.patch
 )
 
 if(VCPKG_TARGET_IS_EMSCRIPTEN)
     set(ENV{EMSCRIPTEN} "$ENV{EMSDK}/upstream/emscripten")
 endif()
-
-# 1. Fix the StatusOr constructor to handle const types (like Eigen Matrices)
-# This changes the error constructor to value-initialize value_, which is required for const types.
-vcpkg_replace_string(
-    "${SOURCE_PATH}/src/draco/core/status_or.h"
-    "StatusOr(const Status &status) : status_(status) {}"
-    "StatusOr(const Status &status) : status_(status), value_() {}"
-)
-
-# 2. Remove 'const' from StatusOr template arguments in trs_matrix.h
-# This prevents the edge case from being triggered by Eigen types.
-vcpkg_replace_string(
-    "${SOURCE_PATH}/src/draco/scene/trs_matrix.h"
-    "StatusOr<const Eigen::Matrix"
-    "StatusOr<Eigen::Matrix"
-)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
