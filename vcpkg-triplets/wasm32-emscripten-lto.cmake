1ba5300c1d14a3c244e3d408a3de3f6c3094c8c8
Fan Gong <gongfan99@hotmail.com>
2025-12-30 09:44:20 -0500

    new file:   vcpkg-triplets/arm64-linux-python.cmake 	new file:   vcpkg-triplets/arm64-osx-python.cmake 	modified:   vcpkg-triplets/wasm32-emscripten-lto.cmake 	new file:   vcpkg-triplets/x64-linux-python.cmake 	new file:   vcpkg-triplets/x64-osx-python.cmake 	new file:   vcpkg-triplets/x64-windows-python.cmake

--- vcpkg-triplets/wasm32-emscripten-lto.cmake
+++ vcpkg-triplets/wasm32-emscripten-lto.cmake
@@ -3,9 +3,10 @@ set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
 
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${EMSCRIPTEN_ROOT}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
 
 set(VCPKG_CXX_FLAGS "-flto=thin")
 set(VCPKG_C_FLAGS "-flto=thin")
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 set(VCPKG_BUILD_TYPE release)
