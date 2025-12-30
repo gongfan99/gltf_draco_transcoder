fb9d276537cc7032fca6869118aa0074fb569352
Fan Gong <gongfan99@hotmail.com>
2025-12-30 10:21:36 -0500

    modified:   vcpkg-triplets/wasm32-emscripten-lto.cmake

--- vcpkg-triplets/wasm32-emscripten-lto.cmake
+++ vcpkg-triplets/wasm32-emscripten-lto.cmake
@@ -3,7 +3,7 @@ set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
 
-set(ENV{EMSCRIPTEN} "$ENV{EMSDK}/upstream/emscripten")
+set(VCPKG_ENV_PASSTHROUGH EMSCRIPTEN)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
 
 set(VCPKG_CXX_FLAGS "-flto=thin")
